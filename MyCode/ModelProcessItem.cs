eebd5e890ff4fa44c635d3a36af9629a5d0b5c34
Koji MARUYAMA <km.ume6@outlook.com>
2024-09-11 16:34:46 +0900

    CollectionViewを使ったデータ作成と、ワーカスレッドの起動・終了を実装した基礎的なバージョン。

---
+++ MyCode/ModelProcessItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masic3.MyCode
+{
+    public class ModelProcessItem
+    {
+        public int ProcessId { get; set; }
+        public TimeSpan ProcessTime { get; set; }
+        public string ProcessTimeToString { get => ProcessTime.ToString(@"hh\:mm"); }
+        public string ProcessCommand { get; set; } = string.Empty;
+        public int ProcessData { get; set; }
+
+        public ModelProcessItem()
+        {
+        }
+
+        public ModelProcessItem(int porecessId, TimeSpan processTime, string processName, int processData)
+        {
+            ProcessId = porecessId;
+            ProcessTime = processTime;
+            ProcessCommand = processName ?? throw new ArgumentNullException(nameof(processName));
+            ProcessData = processData;
+        }
+
+        public ModelProcessItem(ModelProcessItem item) : this(item.ProcessId, item.ProcessTime, item.ProcessCommand, item.ProcessData)
+        {
+        }
+
+    }
+}
--- MyCode/ViewModelProcess.cs
+++ MyCode/ViewModelProcess.cs
@@ -1,21 +1,164 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml.Serialization;
 
 namespace masic3.MyCode
 {
     internal partial class ViewModelProcess : ObservableObject
     {
+        [ObservableProperty] private ObservableCollection<ModelProcessItem> _processItems;
+        [ObservableProperty] private ModelProcessItem? _selectedProcessItem;
+        ModelProcessItem currentProcessItem;
+
+        // これらは入力用コントロールへバインドされる
+        [ObservableProperty] int _editProcessId;
+        [ObservableProperty] TimeSpan _editProcessTime;
+        [ObservableProperty] string _editProcessName = string.Empty;
+        [ObservableProperty] int _editProcessData;
+        [ObservableProperty] bool _isChecked1;
+        
         [ObservableProperty] public string _myMessage = string.Empty;
+        public ICommand BindSelectionChanged { get; private set; }
+
         public ViewModelProcess()
         {
+            ProcessItems = new();
+            SelectedProcessItem = null;
+
+            // バインディング用のコマンド作成
+            BindSelectionChanged = new Command(SelectionChangedCommand);
+
+            // プロセスデータ読込み
+            string xmlFile = MakeXmlName();
+            if (System.IO.File.Exists(xmlFile))
+            {
+                var mySerializer = new XmlSerializer(typeof(ObservableCollection<ModelProcessItem>));
+                using var myFileStream = new FileStream(xmlFile, FileMode.Open);
+                if (myFileStream != null)
+                {
+                    var datas = mySerializer.Deserialize(myFileStream);
+                    if (datas != null)
+                    {
+                        ProcessItems = (ObservableCollection<ModelProcessItem>)datas;
+                    }
+                }
+            }
+
             ThreadPool.QueueUserWorkItem(ProcessThread);
         }
 
+        void SelectionChangedCommand()
+        {
+            if (SelectedProcessItem != null)
+            {
+                EditProcessId = SelectedProcessItem.ProcessId;
+                EditProcessTime = SelectedProcessItem.ProcessTime;
+                EditProcessName = SelectedProcessItem.ProcessCommand;
+                EditProcessData = SelectedProcessItem.ProcessData;
+
+                currentProcessItem = SelectedProcessItem;
+                SelectedProcessItem = null;
+            }
+        }
+
+        [RelayCommand]
+        void AddProcessItem()
+        {
+            currentProcessItem = null;
+            ProcessItems.Add(new ModelProcessItem(ProcessItems.Count + 1, EditProcessTime, EditProcessName, EditProcessData));
+        }
+
+        [RelayCommand]
+        void UpdateProcessItem()
+        {
+            if (currentProcessItem != null)
+            {
+                int index = ProcessItems.IndexOf(currentProcessItem);
+                if (index > -1)
+                {
+                    ProcessItems.RemoveAt(index);
+                    ProcessItems.Insert(index, new ModelProcessItem(index + 1, EditProcessTime, EditProcessName, EditProcessData));
+                }
+            }
+        }
+
+        [RelayCommand]
+        void RemoveProcessItem()
+        {
+            if (currentProcessItem != null)
+            {
+                int index = ProcessItems.IndexOf(currentProcessItem);
+                if (index > -1)
+                {
+                    ProcessItems.RemoveAt(index);
+
+                    ObservableCollection<ModelProcessItem> processItems = new(ProcessItems);
+                    ProcessItems.Clear();
+
+                    ModelProcessItem item;
+                    for (int i = 0; i < processItems.Count; i++)
+                    {
+                        item = processItems[i];
+
+                        if (i >= index)
+                        {
+                            item.ProcessId--;
+                        }
+
+                        ProcessItems.Add(new ModelProcessItem(item));
+                    }
+
+                    processItems.Clear();
+                }
+            }
+        }
+        
+        /// <summary>
+        /// アプリ終了処理
+        /// ・CollectionViewの内容をファイルに保存する
+        /// </summary>
+        [RelayCommand]
+        void Finalization()
+        {
+            // データ書込み（保存）
+            XmlSerializer mySerializer = new XmlSerializer(typeof(ObservableCollection<ModelProcessItem>));
+            StreamWriter myWriter = new StreamWriter(MakeXmlName());
+            mySerializer.Serialize(myWriter, ProcessItems);
+            myWriter.Close();
+
+            Application.Current?.Quit();
+        }
+
+        /// <summary>
+        /// 実行ファイル名の拡張子をxmlに変えた文字列を返す
+        /// </summary>
+        /// <returns></returns>
+        private string MakeXmlName()
+        {
+            string exeName = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            return Path.ChangeExtension(exeName, "xml");
+        }
+
+        void ClearEditProcessItem()
+        {
+            EditProcessId = 0;
+            EditProcessTime = TimeSpan.Zero;
+            EditProcessName = string.Empty;
+            EditProcessData = 0;
+        }
+
+        /// <summary>
+        /// ワーカースレッド
+        /// </summary>
+        /// <param name="state"></param>
         private void ProcessThread(object? state)
         {
             Debug.WriteLine("スレッド開始！");
@@ -23,7 +166,7 @@ namespace masic3.MyCode
 
             do
             {
-                MyMessage = $"Wellcom at {DateTime.Now}";
+                MyMessage = $"Welcome at {DateTime.Now}";
 
                 Thread.Sleep(1000);
             } while (masic3.MyCode.ModelShare.EnableThreadLoop == true);
