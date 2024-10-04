b5e82c6b88151ebe13caae9be273441e86b645d5
Koji MARUYAMA <km.ume6@outlook.com>
2024-10-04 18:21:44 +0900

    KP用コード入力開始！

---
+++ MyCode/KP2000.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masic3.MyCode
+{
+    internal class KP2000 : IDisposable
+    {
+        SerialPort port = new SerialPort();
+        ModbusAscii frame = new ModbusAscii();
+        private bool disposedValue;
+
+        public bool IsOpen => port.IsOpen;
+
+        public void Close()
+        {
+            if (port.IsOpen == true)
+            {
+                port.Close();
+            }
+        }
+
+        public void Init()
+        {
+            port.PortName = Properties.Settings.Default.SerialPort;
+            port.BaudRate = 38400;
+            port.Parity = Parity.Even;
+            port.DataBits = 7;
+            port.StopBits = StopBits.One;
+            port.Handshake = Handshake.None;
+            port.ReadTimeout = 100;
+            port.WriteTimeout = 100;
+        }
+
+        string Communicate(string wFrame)
+        {
+            if (Properties.Settings.Default.EnabledKP != true || wFrame == "") { return ""; }
+
+            string copyFrame = wFrame, readFrame = "";
+            char[] buff = new char[32];
+            int readCnt = 0, limitLoop = 0, limitRetry = 0;
+            bool loopFlag = true;
+            while (loopFlag == true)
+            {
+                if (IsOpen)
+                {
+                    if (copyFrame != "")
+                    {
+                        port.Write(copyFrame);
+                        copyFrame = "";
+                        readFrame = "";
+                    }
+                    else
+                    {
+                        if (port.BytesToRead > 0)
+                        {
+                            try
+                            {
+                                readCnt = port.Read(buff, 0, port.BytesToRead % (buff.Length - 1));
+                                readFrame += new string(buff.Take(readCnt).ToArray());
+                                if (readFrame.Contains("\n") == true)
+                                {
+                                    // 改行が来たら終わりだが、先頭が":"ではない場合はやり直す！
+                                    if (readFrame[0] == ':')
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        copyFrame = wFrame;
+                                    }
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Debug.WriteLine(e.Message);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Init();
+                    port.Open();
+                }
+
+                if (limitLoop++ == 10)
+                {
+                    if (limitRetry++ < 3)
+                    {
+                        limitLoop = 0;
+                        copyFrame = wFrame;
+                    }
+                    else
+                    {
+                        loopFlag = false;
+                        readFrame = "";
+                    }
+                }
+
+                Thread.Sleep(50);
+            }
+
+            return readFrame;
+        }
+
+        public (bool, int) GetSingleData(int sa, int fc, int rn, int dn)
+        {
+            string retStr = Communicate(frame.MakeWriteFrame(sa, fc, rn, dn));
+            if (frame.IsError(retStr) != true)
+            {
+                return (true, Convert.ToInt32(retStr.Substring(7, 4), 16));
+            }
+            else
+            {
+                return (false, -1);
+            }
+        }
+
+        public bool SetSingleData(int sa, int fc, int rn, int dn)
+        {
+            bool retBool = true;
+            string retStr = Communicate(frame.MakeWriteFrame(sa, fc, rn, dn));
+            if (frame.IsError(retStr) != true)
+            {
+                retBool = false;
+            }
+
+            return retBool;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                }
+
+                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+                // TODO: 大きなフィールドを null に設定します
+                if (port.IsOpen == true) { port.Close(); }
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+        ~KP2000()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
---
+++ MyCode/ModbusAscii.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace masic3.MyCode
+{
+    internal class ModbusAscii
+    {
+        public string ReadFrame { get; set; } = "";
+        public string WriteFrame { get; set; } = "";
+        byte[] MsgBuffer = new byte[6];
+
+        /// <summary>
+        /// 伝送データを作る（バイナリ）
+        /// </summary>
+        /// <param name="sa">スレーブアドレス</param>
+        /// <param name="fc">ファンクションコード</param>
+        /// <param name="rn">開始番号</param>
+        /// <param name="dn">データ個数</param>
+        /// <returns>メッセージ本体</returns>
+        public byte[] MakeTransData(int sa, int fc, int rn, int dn)
+        {
+            MsgBuffer[0] = (byte)sa;
+            MsgBuffer[1] = (byte)fc;
+            MsgBuffer[2] = (byte)((rn & 0xff00) >> 8);
+            MsgBuffer[3] = (byte)(rn & 0x00ff);
+            MsgBuffer[4] = (byte)((dn & 0xff00) >> 8);
+            MsgBuffer[5] = (byte)(dn & 0x00ff);
+
+            return MsgBuffer;
+        }
+
+        /// <summary>
+        /// チェックデータを作る(LRC)
+        /// </summary>
+        /// <param name="data">伝送データ</param>
+        /// <returns>チェックデータ</returns>
+        byte MakeLRC(byte[] data)
+        {
+            byte lrc = 0;
+            foreach (byte b in data)
+            {
+                lrc += b;
+            }
+            lrc ^= 0xff;
+
+            return ++lrc;
+        }
+
+        /// <summary>
+        /// メッセージフレーム作成
+        /// </summary>
+        /// <param name="sa"></param>
+        /// <param name="fc"></param>
+        /// <param name="rn"></param>
+        /// <param name="dn"></param>
+        /// <returns></returns>
+        public string MakeWriteFrame(int sa, int fc, int rn, int dn)
+        {
+            byte[] msg = MakeTransData(sa, fc, rn, dn);   // 伝送データ作成
+
+            // ASCII変換
+            WriteFrame = ":";
+            foreach (byte b in MakeTransData(sa, fc, rn, dn))
+            {
+                WriteFrame += $"{b:X2}";
+            }
+
+            WriteFrame += $"{MakeLRC(msg):X2}\r\n"; // エラーチェック付加
+
+            return WriteFrame;
+        }
+
+        public byte[] GetBinaryMSG(string str)
+        {
+            int headPos = str.LastIndexOf(":") + 1;
+            string sub = str.Substring(headPos, str.Length - (headPos + ("\r\n").Length));    // 伝送データ部分（先頭[:]と末尾[CR][LF]以外）を抜き出す
+
+            // 伝送データを２文字ずつに区切ってバイナリ配列に変換
+            byte[] decode = new byte[(sub.Length) / 2];
+            int i = 0;
+            foreach (Match m in Regex.Matches(sub, "(..)"))
+            {
+                decode[i++] = Convert.ToByte(m.Value, 16);
+            }
+
+            return decode;
+        }
+
+        /// <summary>
+        /// 受信フレームのエラーチェック
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>エラーのときはtrue</returns>
+        public bool IsError(string str)
+        {
+            bool judge = true;
+            if (str != "")
+            {
+                byte[] decode = GetBinaryMSG(str);
+                if (decode[1] >= 0x80)
+                {
+                    Debug.Print($"エラー応答！コード：{decode[2]:X2}");
+                }
+
+                // エラーチェック照合
+                byte[] msg = new byte[(decode.Length) - 1];
+                Array.Copy(decode, msg, (decode.Length) - 1);
+                if (decode[decode.Length - 1] == MakeLRC(msg))
+                {
+                    judge = false;
+                }
+                else
+                {
+                    Debug.Print($"受信文字列エラー：{str}");
+                }
+            }
+
+            return judge;
+        }
+
+        /// <summary>
+        /// メッセージフレームを読みやすい文字列に変換
+        /// </summary>
+        /// <param name="buff"></param>
+        /// <returns></returns>
+        public string ParseFrame(string buff)
+        {
+            char[] cary = buff.ToCharArray();
+            string str = "";
+            int code;
+            for (int i = 0; i < cary.Length; i++)
+            {
+                if (i == 0)
+                {
+                    str = $"[{cary[i]}]";
+                }
+                else if (i >= cary.Length - 2)
+                {
+                    code = cary[i];
+                    str += $"[{code:X2}]";
+                }
+                else
+                {
+                    str += cary[i];
+                }
+            }
+
+            return str;
+        }
+    }
+}
